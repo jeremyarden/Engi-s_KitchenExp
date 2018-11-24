fcb3e7d68cd9289a4d7d826916198c707de8cb15
Jeremy Arden <ardenjeremy@gmail.com>
2018-11-25 04:14:01 +0700

    Add files via upload

--- map.c
+++ map.c
@@ -4,78 +4,70 @@
 #include <stdio.h>
 
 char CC;
+boolean EOP;
+
+static FILE * pita;
+static int retval;
+
+void START(FILE *fp) {
+/* Mesin siap dioperasikan. Pita disiapkan untuk dibaca.
+   Karakter pertama yang ada pada pita posisinya adalah pada jendela.
+   I.S. : sembarang
+   F.S. : CC adalah karakter pertama pada pita. Jika CC != MARK maka EOP akan padam (false).
+          Jika CC = MARK maka EOP akan menyala (true) */
+
+	/* Algoritma */
+	pita = fp;
+	ADV();
+}
+
+void ADV() {
+/* Pita dimajukan satu karakter. 
+   I.S. : Karakter pada jendela = 
+          CC, CC != MARK
+   F.S. : CC adalah karakter berikutnya dari CC yang lama, 
+          CC mungkin = MARK.
+		  Jika  CC = MARK maka EOP akan menyala (true) */
+
+	/* Algoritma */
+	retval = fscanf(pita,"%c",&CC);
+	EOP = (CC == MARK);
+	if (EOP) {
+       fclose(pita);
+ 	}
+}
+
 
 void FileToMap(FILE *fp, MATRIKS *room)
 {
-    MATRIKS room1, room2, room3, kitchen;
     indeks i, j;
 
-    MakeMATRIKS(8, 8, &room1);
-    MakeMATRIKS(8, 8, &room2);
-    MakeMATRIKS(8, 8, &room3);
-    MakeMATRIKS(8, 8, &kitchen);
+    MakeMATRIKS(8, 8, room);
 
-    START();
-    for (i = 1; i <= NBrsEff(room1); i++)
+    START(fp);
+    for (i = 1; i <= NBrsEff(*room); i++)
     {
-        for (j = 1; j <= NKolEff(room1); j++)
+        for (j = 1; j <= NKolEff(*room); j++)
         {
-            Elmt(room1, i, j) = CC;
+            Elmt(*room, i, j) = CC;
             ADV();
             //printf("%d", Elmt(room1, i, j));
         }
         ADV();
         //printf("\n");
     }
-
-    *room = room1;
 }
 
-void moveplayer(player *p, MATRIKS room, char cmd){
-    if(cmd == 'w'){
-        if(( Elmt(room, (p->py)-1, p->px) == 'X' ) || ((Elmt(room, (p->py)-1, p->px) - '1' >= 0) && (Elmt(room, (p->py)-1, p->px) - '1' <= 3)) || (p->py-1 < 0)){ //kondisi dia gabisa gerak
-            printf("gabisa gerak kesitu\n");
-        }
-        else{
-            p->py--;
-        }
-
-    }
-
-    if(cmd == 'a'){
-        if(( Elmt(room, p->py, p->px-1) == 'X' ) || ((Elmt(room, p->py, p->px-1) - '1' >= 0) && (Elmt(room, p->py, p->px-1) - '1' <= 3)) || (p->px-1 < 0)){ //kondisi dia gabisa gerak
-            printf("gabisa gerak kesitu\n");
-        }
-        else{
-            p->px--;
-        }
-    }
+void InitMap(MATRIKS *room1, MATRIKS *room2, MATRIKS *room3, MATRIKS *kitchen)
+{
+    FILE *fp;
     
-    if(cmd == 'd'){
-        if(( Elmt(room, p->py, p->px+1) == 'X' ) || ((Elmt(room, p->py, p->px+1) - '1' >= 0) && (Elmt(room, p->py, p->px+1) - '1' <= 3)) || (p->px+1 > NKolEff(room))){ //kondisi dia gabisa gerak
-            printf("gabisa gerak kesitu\n");
-        }
-        else{
-            p->px++;
-        }
-    }
-
-    if(cmd == 's'){
-        if(( Elmt(room, p->py+1, p->px) == 'X' ) || ((Elmt(room, p->py+1, p->px) - '1' >= 0) && (Elmt(room, p->py+1, p->px) - '1' <= 3)) || (p->py+1 > NBrsEff(room))){ //kondisi dia gabisa gerak
-            printf("gabisa gerak kesitu\n");
-        }
-        else{
-            p->py++;
-        }
-    }
-}
-
-void NearestCust(player p, MATRIKS room, int *ordable){
-    char up, down, right, left;
-    
-    *ordable = Elmt(room, p.py, p.px) - '`';
-
-    if((*ordable > 4) || (*ordable < 1)){
-        *ordable = -999;
-    }
+    fp = fopen("room1.txt", "r");
+    FileToMap(fp, room1);
+    fp = fopen("room2.txt", "r");
+    FileToMap(fp, room2);
+    fp = fopen("room3.txt", "r");
+    FileToMap(fp, room3);
+    fp = fopen("kitchen.txt", "r");
+    FileToMap(fp, kitchen);
 }
