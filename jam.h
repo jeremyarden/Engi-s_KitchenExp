79b4bdd57b106bc05f10590951cec32f1fefab7b
marsathoriq <45200071+marsathoriq@users.noreply.github.com>
2018-11-26 01:40:02 +0700

    Update jam.h

--- jam.h
+++ jam.h
@@ -36,21 +36,6 @@ JAM MakeJAM (int HH, int MM, int SS);
 /* ***************************************************************** */
 /* KELOMPOK BACA/TULIS                                               */
 /* ***************************************************************** */
-void BacaJAM (JAM * J);
-/* I.S. : J tidak terdefinisi */
-/* F.S. : J terdefinisi dan merupakan jam yang valid */
-/* Proses : mengulangi membaca komponen HH, MM, SS sehingga membentuk J */
-/* yang valid. Tidak mungkin menghasilkan J yang tidak valid. */
-/* Pembacaan dilakukan dengan mengetikkan komponen HH, MM, SS
- dalam satu baris, masing-masing dipisahkan 1 spasi, diakhiri enter. */
-/* Jika JAM tidak valid maka diberikan pesan: "Jam tidak valid", dan pembacaan
- diulangi hingga didapatkan jam yang valid. */
-/* Contoh:
- 60 3 4
- Jam tidak valid
- 1 3 4
- --> akan terbentuk JAM <1,3,4> */
-
 void TulisJAM (JAM J);
 /* I.S. : J sembarang */
 /* F.S. :   Nilai J ditulis dg format HH:MM:SS */
@@ -71,30 +56,6 @@ JAM DetikToJAM (long N);
  mewakili jumlah detik yang mungkin dalam 1 hari, yaitu dengan rumus:
  N1 = N mod 86400, baru N1 dikonversi menjadi JAM */
 
-/* ***************************************************************** */
-/* KELOMPOK OPERASI TERHADAP TYPE                                    */
-/* ***************************************************************** */
-/* *** Kelompok Operator Relational *** */
-boolean JEQ (JAM J1, JAM J2);
-/* Mengirimkan true jika J1=J2, false jika tidak */
-boolean JNEQ (JAM J1, JAM J2);
-/* Mengirimkan true jika J1 tidak sama dengan J2 */
-boolean JLT (JAM J1, JAM J2);
-/* Mengirimkan true jika J1<J2, false jika tidak */
-boolean JGT (JAM J1, JAM J2);
-/* Mengirimkan true jika J1>J2, false jika tidak */
-/* *** Operator aritmatika JAM *** */
 JAM NextDetik (JAM J);
 /* Mengirim 1 detik setelah J dalam bentuk JAM */
-JAM NextNDetik (JAM J, int N);
-/* Mengirim N detik setelah J dalam bentuk JAM */
-JAM PrevDetik (JAM J);
-/* Mengirim 1 detik sebelum J dalam bentuk JAM */
-JAM PrevNDetik (JAM J, int N);
-/* Mengirim N detik sebelum J dalam bentuk JAM */
-/* *** Kelompok Operator Aritmetika *** */
-long Durasi (JAM JAw, JAM JAkh);
-/* Mengirim JAkh-JAw dlm Detik, dengan kalkulasi */
-/* Jika JAw > JAkh, maka JAkh adalah 1 hari setelah JAw */
-
 #endif
