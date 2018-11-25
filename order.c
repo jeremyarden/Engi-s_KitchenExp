b9b731ca158fdd512bd58141c6aa26b652bb311a
abiyyu2301 <43652262+abiyyu2301@users.noreply.github.com>
2018-11-26 01:09:40 +0700

    Update order.c

--- order.c
+++ order.c
@@ -48,24 +48,24 @@ void DelOrd(TabOrd *T, IdxType idx){
     }
 }
 
-void order(TabOrd *T, MATRIKS M, Kata * arr_makanan){
+void order(player p,TabOrd *T, MATRIKS M, Kata * arr_makanan){
     //matriks itu buat
     //Variables
     //Stack ord adalah stack yang berisi orderan
     //Matriks M adalah board ruangan
-    int rand_ord; //adalah hasil random buat tau orderan
+    int rand_ord,ordable; //adalah hasil random buat tau orderan
     time_t t; //biarin aja yg ini, pake buat random soalnye
     
     srand((unsigned) time(&t)); // buat random 
     //Algo
     //Cek apakah lu bisa ngambil order sekarang
-    if((/*posisi player berada di penanda*/) && (/*meja bisa diambil orderannya*/)){
+    NearestCust(p, M, &ordable);
+    if((ordable>=1) && (ordable<=9)){
 
         rand_ord = rand();//take order
         while((rand_ord < 0) || (rand_ord > 7)){
             rand_ord = rand();
         }
-        
         //tandain kalo meja ini gabisa ngambil order lagi
         
         //masukin order ke TabOrd
