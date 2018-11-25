5ea9e76e5cb02b2d91836ae95a41c63bcb7430b6
marsathoriq <45200071+marsathoriq@users.noreply.github.com>
2018-11-26 01:32:38 +0700

    Update put.c

--- put.c
+++ put.c
@@ -18,28 +18,27 @@ void Put(Stack *hand,Stack *food,BinTree *makanan,player p,MATRIKS kitchen,TabMe
     boolean cektree=false;
     int cnt=0;
     BinTree P;
-    balik(hand,save);  
-    if (CekNampanAda(p,kitchen)&&(!StackIsEmpty(*save))){
-        Pop(save,&temp);
-        if (IsKataEq(temp.str, TM.TM[Akar(*makanan)].nama)){
-            P = *makanan;
-            while (!StackIsEmpty(*save)&&(!cektree)){
+    balik(hand,save);   //stack dibalik dulu karena mengambilnya dari paling atas dalam tree  
+    if (CekNampanAda(p,kitchen)&&(!StackIsEmpty(*save))){   //cek nampan dan save apakah kosong
+        Pop(save,&temp);    //klo tidak kosong ambil nilai
+        if (IsKataEq(temp.str, TM.TM[Akar(*makanan)].nama)){    //cek apakah sama
+            P = *makanan;       
+            while (!StackIsEmpty(*save)&&(!cektree)){           //cari hingga salah bahan atau hingga stack save habis
                 Pop(save,&temp);
-                if (Right(P)!=Nil){
-                    if (IsKataEq(TM.TM[Akar(Right(P))].nama, temp.str)){
+                if (Right(P)!=Nil){         //cek bagian kanan
+                    if (IsKataEq(TM.TM[Akar(Right(P))].nama, temp.str)){   
                         P=Right(P);
                     }
-                }else if (Left(P)!=Nil){
+                }else if (Left(P)!=Nil){    //cek bagian kiri
                     if (IsKataEq(TM.TM[Akar(Left(P))].nama, temp.str)){
                         P=Left(P);
                     }
-                }else{
+                }else{          //kalau gagal
                     cektree=true;
                 }
             }
         }
-        if (!cektree&&StackIsEmpty(*save)){
-            P = *makanan;
+        if (!cektree&&StackIsEmpty(*save)){ //jika stack kosong dan belum menemukan salah bahan   
             temp.str = TM.TM[Akar(Left(P))].nama;
             temp.harga = TM.TM[Akar(Left(P))].harga;
             Push(food,save);
