d8a892312c164a98e015b6f5911bbfbe65aef317
marsathoriq <45200071+marsathoriq@users.noreply.github.com>
2018-11-26 01:05:40 +0700

    Update give.c

--- give.c
+++ give.c
@@ -7,27 +7,29 @@
 #include "order.c"
 #include <stdlib.h>
 boolean ceksekitar(player p,MATRIKS room1,MATRIKS room2,MATRIKS room3);
+int mejaKe(p,room1,room2,room3);
 void Give(Stack *food,TabOrd *order,int uang,player p,TabMeja *TM,MATRIKS *room1,MATRIKS *room2,MATRIKS *room3){ //T isinya nama makanan,harga
 	infotype temp,sampah;
 	IdxType idx;
-	int price;
-	if (ceksekitar(p,*room1,*room2,*room3)){
-		Pop(food,&temp);
+	int price,table;
+	if (ceksekitar(p,*room1,*room2,*room3)){	
+		Pop(food,&temp);	//pop makanan
 		//search masih belum bener
-		if (SearchOrd(*order,temp.str)!=-999){
-			if (p.room==1){
-				kosongmeja(TM,room1,Search2(*order,temp.str));
-			}else if (p.room==2){
-				kosongmeja(TM,room2,Search2(*order,temp.str));
-			}else if (p.room==3){
-				kosongmeja(TM,room3,Search2(*order,temp.str));
-			}		
+		table=mejaKe(p,*room1,*room2,*room3);
+		if (SearchOrd(*order,temp.str)==table){		//jika pesanan benar		
+			if (Room(p)==1){
+				kosongmeja(TM,room1,SearchOrd(*order,temp.str));
+			}else if (Room(p)==2){
+				kosongmeja(TM,room2,SearchOrd(*order,temp.str));
+			}else if (Room(p)==3){
+				kosongmeja(TM,room3,SearchOrd(*order,temp.str));
+			}		//kosongkan meja
 		//set info queue order
-			idx=SearchOrd(*order,temp.str);
-			price=temp.harga;
-			uang+=price;
+			idx=SearchOrd(*order,temp.str);	//cari indeks yang akan dihapus di order
+			price=temp.harga;	//harga
+			uang+=price;		//uang bertambah
 			//hapus orderan
-			Del(order,idx);
+			DelOrd(order,idx);		//menghapus order indeks ke idx
 		}else{
 			Push(food,temp);
 			printf("Pesanan salah kirim\n");
@@ -35,22 +37,31 @@ void Give(Stack *food,TabOrd *order,int uang,player p,TabMeja *TM,MATRIKS *room1
 	}else{
 		printf("Posisi salah\n");
 	}
-	if (p.room==1){
+	if (Room(p)==1){
 		TulisMATRIKS(*room1);
-	}else if (p.room==2){
+	}else if (Room(p)==2){
 		TulisMATRIKS(*room2);
-	}else if (p.room==3){
+	}else if (Room(p)==3){
 		TulisMATRIKS(*room3);
 	}
 }
 boolean ceksekitar(player p,MATRIKS room1,MATRIKS room2,MATRIKS room3){
-	if (p.room==1){
+	if (Room(p)==1){
 		return ((room1[X(p)][Y(p)]>='a')&&(room1[X(p)][Y(p)]<='j'));
-	}else if (p.room==2){
+	}else if (Room(p)==2){
 		return (room2[X(p)][Y(p)]>='a'&&room2[X(p)][Y(p)]<='j');
-	}else if (p.room==3){
+	}else if (Room(p)==3){
 		return (room3[X(p)][Y(p)]>='a'&&room3[X(p)][Y(p)]<='j');
 	}else{
 		return false;
+	}
+}
+int mejaKe(p,room1,room2,room3){
+	if (Room(p)==1){
+		return room1[X(p)][Y(p)]-'a'+1;
+	}else if (Room(p)==2){
+		return room2[X(p)][Y(p)]-'a'+1;
+	}else if (Room(p)==3){
+		return room3[X(p)][Y(p)]-'a'+1;
 	}
 }
