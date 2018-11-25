984f0f596bef79257a581c8756667da3cf6a5f90
abiyyu2301 <43652262+abiyyu2301@users.noreply.github.com>
2018-11-25 14:44:42 +0700

    Update stackt.h

--- stackt.h
+++ stackt.h
@@ -6,31 +6,27 @@
 #define stackt_H
 
 #include "boolean.h"
-#include "mesinkata.h" // Buat mesin kata
+#include "mesinkata.h"
 #define Nil 0
 #define MaxEl 100
 /* Nil adalah stack dengan elemen kosong . */
 /* Karena indeks dalam bhs C dimulai 0 maka tabel dg indeks 0 tidak dipakai */
-typedef struct 	{ 
-	char str[MaxEl+1];
-	int Neff;			  
-	} string;		
 typedef struct{
-	string str;
-	int harga;
-}infotype;
+    Kata str;
+    int harga;
+}infotypeStack;
 typedef int address;   /* indeks tabel */
 /* Contoh deklarasi variabel bertype stack dengan ciri TOP : */
 /* Versi I : dengan menyimpan tabel dan alamat top secara eksplisit*/
-typedef struct { 
-	infotype T[MaxEl+1]; /* tabel penyimpan elemen */
-	address TOP;  /* alamat TOP: elemen puncak */
+typedef struct {
+    infotypeStack T[MaxEl+1]; /* tabel penyimpan elemen */
+    address TOP;  /* alamat TOP: elemen puncak */
 } Stack;
 /* Definisi stack S kosong : S.TOP = Nil */
 /* Elemen yang dipakai menyimpan nilai Stack T[1]..T[MaxEl] */
 /* Jika S adalah Stack maka akses elemen : */
-   /* S.T[(S.TOP)] untuk mengakses elemen TOP */
-   /* S.TOP adalah alamat elemen TOP */
+/* S.T[(S.TOP)] untuk mengakses elemen TOP */
+/* S.TOP adalah alamat elemen TOP */
 
 /* Definisi akses dengan Selektor : Set dan Get */
 #define Top(S) (S).TOP
@@ -51,13 +47,13 @@ boolean StackIsFull (Stack S);
 /* Mengirim true jika tabel penampung nilai elemen stack penuh */
 
 /* ************ Menambahkan sebuah elemen ke Stack ************ */
-void Push (Stack * S, infotype X);
+void Push (Stack * S, infotypeStack X);
 /* Menambahkan X sebagai elemen Stack S. */
 /* I.S. S mungkin kosong, tabel penampung elemen stack TIDAK penuh */
 /* F.S. X menjadi TOP yang baru,TOP bertambah 1 */
 
 /* ************ Menghapus sebuah elemen Stack ************ */
-void Pop (Stack * S, infotype* X);
+void Pop (Stack * S, infotypeStack* X);
 /* Menghapus X dari Stack S. */
 /* I.S. S  tidak mungkin kosong */
 /* F.S. X adalah nilai elemen TOP yang lama, TOP berkurang 1 */
